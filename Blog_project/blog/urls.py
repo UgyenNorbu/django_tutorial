f01d9b84e525c47cb956282abf4eb28c225da15a
Ugyen Norbu <unorbu2019@gmail.com>
2024-06-09 10:38:52 +0600

    Changes in user Login and Log out

--- Blog_project/blog/urls.py
+++ Blog_project/blog/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog_app.urls')),
 ]
